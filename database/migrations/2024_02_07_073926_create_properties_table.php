a08c71a034240b8312a63285b1b5284a10ac1fc4
regisatl <regisattolou21@outlook.fr>
2024-02-07 08:45:55 +0100

    Ajout des champs dans les migrations et migrations effectuée pour la table properties

--- database/migrations/2024_02_07_073926_create_properties_table.php
+++ database/migrations/2024_02_07_073926_create_properties_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('properties', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->longText('description');
+            $table->integer('surface');
+            $table->integer('rooms');
+            $table->integer('bedrooms');
+            $table->integer('floor');
+            $table->integer('price');
+            $table->string('city');
+            $table->string('address');
+            $table->string('postal_code');
+            $table->boolean('sold');
             $table->timestamps();
         });
     }
