64aa141be9af67a98c7c10a360ea086535b42c0a
regisatl <regisattolou21@outlook.fr>
2024-02-14 17:46:15 +0100

    fonction index du controller home ajoutée

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Property;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index() {
-        return view('welcome');
+        $properties = Property::orderBy('created_at', 'desc')->limit(4)->get();
+        return view('home', compact('properties'));
     }
 }
