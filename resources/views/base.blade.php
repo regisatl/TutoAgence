d6d7fc8d4da5a652be7b62f9da32bb80984509ec
regisatl <regisattolou21@outlook.fr>
2024-02-14 18:09:36 +0100

    fin de la vue home

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -10,9 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/css/tom-select.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/js/tom-select.complete.min.js"></script>
-    <title>@yield('title') | Administration</title>
+    <title>@yield('title') | Agence Immo</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         body {
@@ -30,10 +28,6 @@
     <div class="mt-5">
         @yield('content')
     </div>
-
-    <script>
-        new TomSelect('select[multiple]', {plugins: {remove_button: {title: 'supprimer'}}})
-    </script>
 
 </body>
 
